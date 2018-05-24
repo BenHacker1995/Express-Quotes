ef761c2337e85fdd3f33ce3702109e9d650bf3cf
Ben Hacker <bhacker1995@gmail.com>
2018-05-24 08:24:48 -0500

    Finally realized I do need AJAX and whatnot :/

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,41 +3,42 @@ console.log('js');
 $( document ).ready( readyNow );
 
 function readyNow() {
-    console.log('JQ');
+    $( 'input' ).empty();
+    getQuotes();
     $( '#quoteButton' ).on( 'click', addQuote );
 }
 
-function addQuote() {
-    let newQuote = {
-        quote: $( '#quoteText' ).val(),
-        author: $( '#quoteAuthor' ).val()
-    };
-    $.ajax({
-        method: 'POST',
-        url: '/quotes',
-        data: newQuote
-    }).then( function ( response ){
-        console.log( 'back from server with ', response );
-        getQuote();
-    });
-    // quotes.push( newQuote ); 
-}
-
-function displayQuotes( quotes ) {
+function quoteDisplay( quotes ) {
     let i = $( '#quoteOutputs');
     i.empty();
-    for ( quotation of quotes ) {
-        i.append( '<li>' + quotation.quote + ' ~' + quotation.author + '</li>')
-    } // end for
-} // end displayQuotes
+    for ( quotation of quotes ){
+        i.append( '<li>' + quotation.quote + ' ~' + quotation.author + '</li>');
+    }
+}
 
-function getQuote() {
-    console.log('in getQuote');
+function getQuotes() {
+    console.log('in getQuotes');
     $.ajax({
         method: 'GET',
         url: '/quotes'
-    }).then( function ( response ) {
-        console.log('back from server with ', response );
-        displayQuotes( response );
+    }).then( function( response ){
+        console.log('from server with', response);
+        quoteDisplay( response );
     });
 }
+
+function addQuote() {
+    let quoteAdd = {
+        quote: $( '#quoteText' ).val(),
+        author: $( '#quoteAuthor' ).val()
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        data: quoteAdd
+    }).then( function ( response ){
+        console.log( 'back from server with ', response );
+    });
+    getQuotes();
+}
+
--- server/server.js
+++ server/server.js
@@ -1,19 +1,37 @@
 // requires
 const express = require( 'express' );
 const app = express();
-// let bodyParser = require( 'body-parser' );
+let bodyParser = require( 'body-parser' );
 // globals
 const port = 5000;
+
 let quotes = [];
 
+let quote1 = {
+    quote: 'sup nerds',
+    author: 'Curtis'
+};
+
+let quote2 = {
+    quote: `It's true blue, Ma!`,
+    author: 'Dev'
+};
+
+let quote3 = {
+    quote: 'Ribbit',
+    author: 'me'
+};
+
+quotes.push( quote1, quote2, quote3 );
 
 
-// uses
+
+// // uses
 app.use( express.static( 'server/public' ) );
 
-// app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
-// server up
+// // server up
 app.listen( port, () => {
     console.log( 'server up on port: ', port );
 }); // end server up
@@ -27,6 +45,6 @@ app.get( '/quotes', ( req, res ) => {
 // post route
 app.post( '/quotes', (req, res) =>{
     console.log( 'in POST hit for /quotes route: ', req.body );
-    quotes.push( req.body.quote, req.body.author );
+    quotes.push( req.body );
     res.sendStatus( 200 );
 });
